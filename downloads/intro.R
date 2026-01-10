8603bd028129204b4be2a7dac3d7432b1cc16474
Sara Souther <sara.souther@nau.edu>
2026-01-09 18:58:57 -0700

    changing intro

--- downloads/intro.R
+++ downloads/intro.R
@@ -6,11 +6,26 @@ library(tidyverse)
 # Remember there are two ways to install packages, the first option is the packages tab on your screen, and the second is using code:
 install.packages("tidyverse")
 
-# Set your own working directory here 
-setwd("your_path_to_your_working_folder") 
+# Set your own working directory 
 
 # Read in a file
-read.csv("your_data.csv")
+data <- read.csv("WelcomeToENV226.csv")
 
-# Write a file
-write.csv(your_object_to_export, "your_file_name.csv") 
+# Look at your raw data
+data
+
+# Let's visualize your data
+ggplot(data, aes(x = DistanceFrHome)) +
+  geom_histogram(bins = 10)
+
+ggplot(data, aes(x = NumberOfRoommates)) +
+  geom_bar()
+
+ggplot(data, aes(x = FavoritePet)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(data, aes(x = DBH)) +
+  geom_histogram(bins = 10) +
+  labs(x = "Diameter at Breast Height (cm)",
+       y = "Number of trees")
